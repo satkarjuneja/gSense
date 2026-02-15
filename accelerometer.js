8602b258a55dcceec075c3b126130286c1f21629
satkarjuneja <satkarjuneja@gmail.com>
2026-02-15 22:40:41 +0530

    starting with basic detection

--- accelerometer.js
+++ accelerometer.js
@@ -1,3 +1,7 @@
+let listening = true; 
+let data = [];
+
+
 if (typeof DeviceMotionEvent.requestPermission === 'function') {
     DeviceMotionEvent.requestPermission()
         .then(response => {
@@ -10,6 +14,17 @@ if (typeof DeviceMotionEvent.requestPermission === 'function') {
         })
         .catch(console.error);
 } else {
-    // No permission API → just listen
     window.addEventListener('devicemotion', handleMotion);
+}
+
+
+function handleMotion(event) {
+    if (!listening) return; // stop collecting if not started
+    const x = event.accelerationIncludingGravity.x;
+    const y = event.accelerationIncludingGravity.y;
+    const z = event.accelerationIncludingGravity.z;
+    const mag = Math.sqrt(x*x + y*y + z*z);
+    data.push(mag);
+
+    document.getElementById('output').innerHTML = `Last sample: ${mag.toFixed(2)} m/s²`;
 }
